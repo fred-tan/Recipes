6475d173260680c227f940446c3a8f818aeab352
Fred-Illiade <frederic.tanon@gmail.com>
2020-12-14 20:16:55 +0100

    Recipe reduxify v2

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,17 @@
 // == Import npm
 import React from 'react';
 import {Route} from 'react-router-dom';
-import slugify from 'slugify';
+
 // == Import
 import Recipe from 'src/containers/Recipe';
 import Navigation from 'src/containers/Navigation';
 import Home from 'src/containers/Home';
 import './styles.css';
-import data from '../../data';
-
-
 
 // == Composant
 const App = () => (
   <div className="app">
-    
+
     <Navigation />
     <main className="main">
     <header className="header">oRecipe</header>
@@ -25,20 +22,6 @@ const App = () => (
 
     <Route path="/recipe/:slug" component={Recipe} />
 
-      {/* <Route path="/recipe/:slug"
-        render={(routerObject) => {
-          console.log(routerObject);
-
-          const {slug} = routerObject.match.params;
-
-          const foundRecipe = data.find((recipeObject) => {
-            const recipeUrl = slugifyTitle(recipeObject.title);
-            const slugUrl = `/recipe/${slug}`
-            return recipeUrl === slugUrl;
-          });
-          return <Recipe recipe={foundRecipe}/>;
-        }}
-      /> */}
     </main>
   </div>
 );
